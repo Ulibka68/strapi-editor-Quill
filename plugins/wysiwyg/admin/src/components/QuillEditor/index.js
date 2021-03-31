dc4ded4e380f7c187329b5b4106412ecbb886844
Ulibka68 <gena68@inbox.ru>
2021-03-31 17:07:47 +0300

    добавил в Quill цвета

--- plugins/wysiwyg/admin/src/components/QuillEditor/index.js
+++ plugins/wysiwyg/admin/src/components/QuillEditor/index.js
@@ -6,13 +6,24 @@ import 'react-quill/dist/quill.snow.css';
 const Editor = ({ onChange, name, value }) => {
 
   const modules = {
+    // toolbar: [
+    //   [{ 'header': '1'}, {'header': '2'}, { 'font': [] }],
+    //   [{size: []}],
+    //   ['bold', 'italic', 'underline','strike', 'blockquote'],
+    //   [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
+    //   ['link'],
+    //   ['clean']
+    // ],
     toolbar: [
-      [{ 'header': '1'}, {'header': '2'}, { 'font': [] }],
-      [{size: []}],
+      [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+      [{ size: [ 'small', false, 'large', 'huge' ]}],
       ['bold', 'italic', 'underline','strike', 'blockquote'],
       [{'list': 'ordered'}, {'list': 'bullet'}, {'indent': '-1'}, {'indent': '+1'}],
-      ['link'],
-      ['clean']
+      ['link', 'image'],
+      [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+      [{ 'font': [] }],
+      [{ 'align': [] }],
+        ['clean']
     ],
   }
 
